ccc420f54f22c0b9a67b77605753e37eef5d4729
Nikhil <nikhil4040k@gmail.com>
2023-11-19 14:26:13 +0530

    -m login and register pages added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
-    
+
